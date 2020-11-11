58ac64d4e9f9ef19b90968db7546a6ca12341913
amandena <manda.m.johnson@gmail.com>
2020-11-10 22:46:35 -0500

    created Jobs and Job components

---
+++ client/src/components/Job.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Job extends React.Component {
+  render() {
+    return(
+      <div>
+        Job
+      </div>
+    )
+  }
+}
+
+export default Job
---
+++ client/src/components/Jobs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Job from './Job'
+
+class Jobs extends React.Component {
+  render() {
+    return(
+      <div>
+        Jobs
+        <Job/>
+      </div>
+    )
+  }
+}
+
+export default Jobs

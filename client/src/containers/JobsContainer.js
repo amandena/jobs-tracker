4403293f50e0478e4254b50afa1a38ff0c7d768d
amandena <manda.m.johnson@gmail.com>
2020-11-10 22:53:56 -0500

    changed back into a class component

--- client/src/containers/JobsContainer.js
+++ client/src/containers/JobsContainer.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Jobs from '../components/Jobs'
 
-const JobsContainer = () => {
-  return(
-    <div>
-      JobsContainer
-      <Jobs/>
-    </div>
-  )
+class JobsContainer extends React.Component {
+  render() {
+    return(
+      <div className='jobs-container'>
+        JobsContainer
+        <Jobs/>
+      </div>
+    )
+  }
 }
 
 export default JobsContainer
+
+//make fetch request here for jobs to pass into Jobs and Job Routes

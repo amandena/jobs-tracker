cba64350c60cd4d6a6030314f151f764c620c560
amandena <manda.m.johnson@gmail.com>
2020-11-10 22:47:04 -0500

    imported React and JobsContainer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
+import React from 'react'
+import JobsContainer from './containers/JobsContainer'
 
 function App() {
   return (
     <div className='App'>
       Jobs Tracker
+      <JobsContainer/>
     </div>
   )
 }

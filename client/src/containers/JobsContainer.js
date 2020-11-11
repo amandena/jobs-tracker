c53120cb76ea0fc9195f48d01f2bdb3224bbdf14
amandena <manda.m.johnson@gmail.com>
2020-11-10 22:46:06 -0500

    created JobsContainer

---
+++ client/src/containers/JobsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Jobs from '../components/Jobs'
+
+const JobsContainer = () => {
+  return(
+    <div>
+      JobsContainer
+      <Jobs/>
+    </div>
+  )
+}
+
+export default JobsContainer

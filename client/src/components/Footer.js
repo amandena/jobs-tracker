b1cbf4f3ca37dc1e2289a3f701b38dc38100a7b1
amandena <manda.m.johnson@gmail.com>
2020-11-11 21:16:54 -0500

    added content to footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SocialMedia from './SocialMedia'
+
+const Footer = () => {
+  return(
+    <div className='footer'>
+      <SocialMedia/>
+      <p>© 2020 Amanda M Johnson</p>
+    </div>
+  )
+}
+
+export default Footer

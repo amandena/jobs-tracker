128e8419ae5d182881468071099f9dd17b5a3c3d
amandena <manda.m.johnson@gmail.com>
2020-11-11 21:16:28 -0500

    created component to display and link to social media sites

---
+++ client/src/components/SocialMedia.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import github from '../assets/social-media/white-gh.png'
+import linkedin from '../assets/social-media/linkedin.png'
+import medium from '../assets/social-media/medium.png'
+
+const SocialMedia = () => {
+  return(
+    <div className='social-media'>
+      <a href='https://github.com/amandena'><img src={github} alt="github" width='25'/></a>
+      <a href='https://www.linkedin.com/in/amandena/'><img src={linkedin} alt="linkedin" width='25'/></a>
+      <a href='https://medium.com/@manda.m.johnson'><img src={medium} className='medium' alt="medium" width='25' /></a>
+    </div>
+  )
+}
+
+export default SocialMedia

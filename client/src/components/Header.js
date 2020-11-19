0a3defe581c33e213c1b238de3ea422888a4a716
amandena <manda.m.johnson@gmail.com>
2020-11-18 23:40:29 -0500

    added a header tag

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return(
     <div className='header'>
+      <h1>Jobs Tracker</h1>
       <Link to='#'>Applications</Link>
       <Link to='#'>Goals</Link>
       <Link to='#'>Add Job</Link>

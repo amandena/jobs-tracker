0e5f67ee8e28ec6e5c4f8113c22339a8de719a16
amandena <manda.m.johnson@gmail.com>
2020-11-19 23:57:26 -0500

    added path to Links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,9 +5,9 @@ const Header = () => {
   return(
     <div className='header'>
       <h1>Jobs Tracker</h1>
-      <Link to='#'>Applications</Link>
-      <Link to='#'>Goals</Link>
-      <Link to='#'>Add Job</Link>
+      <Link to='/applications-tracker'>Applications</Link>
+      <Link to='/goals'>Goals</Link>
+      <Link to='/jobs/new'>Add Job</Link>
     </div>
   )
 }

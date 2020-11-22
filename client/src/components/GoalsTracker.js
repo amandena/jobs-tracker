fffb5ad0b969d989ddb383feae50a155a02d3401
amandena <manda.m.johnson@gmail.com>
2020-11-21 20:37:59 -0500

    added fields for things to keep track of

--- client/src/components/GoalsTracker.js
+++ client/src/components/GoalsTracker.js
@@ -3,7 +3,15 @@ import React from 'react'
 const GoalsTracker = () => {
   return(
     <div>
-      GoalsTracker
+      <p>${} Applications for week of ${}</p>
+
+      <p>Jobs Applied to: </p>
+      <p>Rejections: </p>
+      <p>Personality Assessments: </p>
+      <p>Code Challenges: </p>
+      <p>HR Interviews: </p>
+      <p>Technical Interviews: </p>
+      <p>Internships: </p>
     </div>
   )
 }

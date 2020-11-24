18715177d0213fee4d776536c8671bc5152b04ad
amandena <manda.m.johnson@gmail.com>
2020-11-24 16:48:00 -0500

    started adding bootstrap markup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return(
-    <div className='header'>
-      <h1>Jobs Tracker</h1>
-      <Link to='/applications-tracker'>Applications</Link>
-      <Link to='/goals'>Goals</Link>
-      <Link to='/jobs/new'>Add Job</Link>
+    <div className="nav navbar-light">
+      <div className="container-fluid">
+        <div className="row mainnav-text text-center">
+          <div className="col-sm-12">
+            <h1>Jobs Tracker</h1>
+            <Link className="mainnav-link" to='/applications-tracker'>Applications</Link>
+            <Link className="mainnav-link" to='/goals'>Goals</Link>
+            <Link className="mainnav-link" to='/jobs/new'>Add Job</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

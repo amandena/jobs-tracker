98a3b44ecb029ea44b2e10cf4583a1b59fc8eb4c
amandena <manda.m.johnson@gmail.com>
2020-12-01 16:49:44 -0500

    added a footer tag

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,14 +3,16 @@ import SocialMedia from './SocialMedia'
 
 const Footer = () => {
   return(
-    <div className='container'>
-      <div className='row spacer'>
-        <div className='col-12 text-center'>
-          <SocialMedia/>
-          <p>&copy; 2020 Amanda M Johnson</p>
+    <footer>
+      <div className='container'>
+        <div className='row spacer'>
+          <div className='col-12 text-center'>
+            <SocialMedia/>
+            <p>&copy; 2020 Amanda M Johnson</p>
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   )
 }
 

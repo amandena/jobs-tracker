60542f388fdc2b3877d00c7456f2ea6b44e26aa7
amandena <manda.m.johnson@gmail.com>
2020-12-01 20:27:22 -0500

    added a header tag

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return(
-    <div className="nav navbar-light">
-      <div className="container-fluid">
-        <div className="row mainnav-text text-center">
-          <div className="col-sm-12">
-            <h1>Jobs Tracker</h1>
-            <Link className="mainnav-link" to='/applications-tracker'>Applications</Link>
-            <Link className="mainnav-link" to='/goals'>Goals</Link>
-            <Link className="mainnav-link" to='/jobs/new'>Add Job</Link>
+    <header>
+      <div className="nav navbar-light">
+        <div className="container-fluid">
+          <div className="row mainnav-text text-center">
+            <div className="col-sm-12">
+              <h1>Jobs Tracker</h1>
+              <Link className="mainnav-link" to='/applications-tracker'>Applications</Link>
+              <Link className="mainnav-link" to='/goals'>Goals</Link>
+              <Link className="mainnav-link" to='/jobs/new'>Add Job</Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </header>
   )
 }
 

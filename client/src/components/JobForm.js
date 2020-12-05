e7d2e8b247538f9b4f7bac9f752c57abccea0fa1
amandena <manda.m.johnson@gmail.com>
2020-12-04 23:41:04 -0500

    started adding markup to form

--- client/src/components/JobForm.js
+++ client/src/components/JobForm.js
@@ -3,53 +3,72 @@ import React from 'react'
 class JobForm extends React.Component {
   render() {
     return(
-      <div className='job-form'>
+      <div className='job-form container'>
+        <div className="spacer"/>
         <form>
-          <input 
-            type='text' 
-            placeholder='Date' 
-            name='date'
-          />
-          <br/>
-          <input 
-            type='text' 
-            placeholder='Company Name' 
-            name='companyName'
-          />
-          <br/>
-          <input
-            type='text'
-            placeholder='Job Title'
-            name='jobTitle'
-          />
-          <br/>
-          <input
-            type='text'
-            placeholder='Application URL'
-            name='applicationUrl'
-          />
-          <br/>
-          <input
-            type='text'
-            placeholder='Referral Source'
-            name='referralSource'
-          />
-          <br/>
-          <input
-            type='textarea'
-            placeholder='Notes'
-            name='notes'
-          />
-          <br/>
-          <input
-            type='text'
-            placeholder='Application Status'
-            name='applicationStatus'
-          />
-          <br/>
-          <input
-            type='submit'
-          />
+          <div className="row form-group">
+            <div className="col-8 offset-2 input-group spacer-xs">
+              <input 
+                type='text' 
+                placeholder='Date' 
+                name='date'
+              />
+            </div>
+            
+            <div className="col-8 offset-2 input-group spacer-xs"></div>
+              <input 
+                type='text' 
+                placeholder='Company Name' 
+                name='companyName'
+              />
+            </div>
+            <br/>
+            <div className="col-8 offset-2 input-group spacer-xs">
+              <input
+                type='text'
+                placeholder='Job Title'
+                name='jobTitle'
+              />
+            </div>
+            <br/>
+            <div className="col-8 offset-2 input-group spacer-xs">
+              <input
+                type='text'
+                placeholder='Application URL'
+                name='applicationUrl'
+              />
+            </div>
+            <br/>
+            <div className="col-8 offset-2 input-group spacer-xs">
+              <input
+                type='text'
+                placeholder='Referral Source'
+                name='referralSource'
+              />
+            </div>
+            <br/>
+            <div className="col-8 offset-2 input-group spacer-xs">
+              <input
+                type='textarea'
+                placeholder='Notes'
+                name='notes'
+              />
+            </div>
+            <br/>
+            <div className="col-8 offset-2 input-group spacer-xs">
+              <input
+                type='text'
+                placeholder='Application Status'
+                name='applicationStatus'
+              />
+            </div>
+            <br/>
+            <div className="col-8 offset-2 input-group spacer-xs">
+              <input
+                type='submit'
+              />
+            </div>
+          {/* </div> */}
         </form>
       </div>
     )

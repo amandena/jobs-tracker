e1e70a182bdcd53793f0609a2f337ed006570fdc
amandena <manda.m.johnson@gmail.com>
2020-12-07 22:20:14 -0500

    added markup to search form

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -2,10 +2,24 @@ import React from 'react'
 
 const Search = () => {
   return(
-    <div className='search'>
+    <div className='search container'>
+      <div className="spacer"/>
       <form>
-        <input type='text'/>
-        <input type='submit' value='Search'/>
+        <div className="row form-group">
+          <div className="col-8 offset-2 input-group spacer-xs">
+            <input 
+              className="form-control input-lg"
+              type='text'
+            />
+          </div>
+          <div className="col-8 offset-2 input-group spacer-xs">
+            <input 
+              className="btn btn-secondary"
+              type='submit' 
+              value='Search'
+            />
+          </div>
+        </div>
       </form>
     </div>
   )

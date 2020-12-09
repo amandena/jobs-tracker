47d69b7fdbe6fd2a1e85fddaaa200d785b24d18a
amandena <manda.m.johnson@gmail.com>
2020-12-08 20:35:17 -0500

    added more markup

--- client/src/components/GoalsTracker.js
+++ client/src/components/GoalsTracker.js
@@ -2,16 +2,20 @@ import React from 'react'
 
 const GoalsTracker = () => {
   return(
-    <div>
-      <p>{} Applications for week of {}</p>
+    <div className="goals container">
+      <div className="row spacer">
+        <div className="col-12">
+          <p>{} Applications for week of {}</p>
 
-      <p>Jobs Applied to: </p>
-      <p>Rejections: </p>
-      <p>Personality Assessments: </p>
-      <p>Code Challenges: </p>
-      <p>HR Interviews: </p>
-      <p>Technical Interviews: </p>
-      <p>Internships: </p>
+          <p>Jobs Applied to: </p>
+          <p>Rejections: </p>
+          <p>Personality Assessments: </p>
+          <p>Code Challenges: </p>
+          <p>HR Interviews: </p>
+          <p>Technical Interviews: </p>
+          <p>Internships: </p>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Job = () => {
   return(
-    <div>
+    <div className="job container">
       Job
     </div>
   )

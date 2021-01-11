a34182fb906aeeae61a24e08010e30ae08b2091c
amandena <manda.m.johnson@gmail.com>
2021-01-11 11:00:02 -0500

    added new case

--- client/src/reducers/mainReducer.js
+++ client/src/reducers/mainReducer.js
@@ -6,6 +6,9 @@ export default function mainReducer(state = {
   searchForm: initialSearchState
 }, action) {
   switch (action.type) {
+    case 'UPDATE_SEARCH_FORM':
+      return {...state, searchForm: action.payload}
+      
     default:
       return state
   }

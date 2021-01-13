ef386fb43267742e041f3609b068b6e52bdbaf42
amandena <manda.m.johnson@gmail.com>
2021-01-13 12:43:56 -0500

    expanded on state

--- client/src/reducers/mainReducer.js
+++ client/src/reducers/mainReducer.js
@@ -2,13 +2,24 @@ const initialSearchState = {
   searchBox: ''
 }
 
+const initialJobState = {
+  date: '',
+  companyName: '',
+  jobTitle: '',
+  applicationUrl: '',
+  referralSource: '',
+  notes: '',
+  applicationStatus: ''
+}
+
 export default function mainReducer(state = {
-  searchForm: initialSearchState
+  searchForm: initialSearchState,
+  jobForm: initialJobState
 }, action) {
   switch (action.type) {
     case 'UPDATE_SEARCH_FORM':
       return {...state, searchForm: action.payload}
-      
+
     default:
       return state
   }

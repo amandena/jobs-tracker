18b66f593dde2cd7bbcde5c060d6f695d1cdfeab
amandena <manda.m.johnson@gmail.com>
2021-01-21 19:48:55 -0500

    added another dispatch case and a redirect

--- client/src/actions/addJob.js
+++ client/src/actions/addJob.js
@@ -1,5 +1,7 @@
-export function addJob(formData) {
+export function addJob(formData, history) {
   return (dispatch) => {
     dispatch({type: 'ADD_JOB', payload: formData})
+    dispatch({type: 'CLEAR_JOB_FORM'})
+    history.push('/applications-tracker')
   }
 }

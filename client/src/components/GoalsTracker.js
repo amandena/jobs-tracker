93054943e1f9cb99a372ed8fbd38b72c3ed5b217
amandena <manda.m.johnson@gmail.com>
2021-01-28 11:58:58 -0500

    added more fields with some hard-coded data and a dynamic date object

--- client/src/components/GoalsTracker.js
+++ client/src/components/GoalsTracker.js
@@ -6,15 +6,17 @@ const GoalsTracker = () => {
       <div className='row spacer'>
         <div className='col-12'>
           <h3>Goals</h3>
-          <p>{} Applications for week of {}</p>
+          <p>1 Applications for week of {new Date().toLocaleDateString()}</p>
 
-          <p>Jobs Applied to: </p>
-          <p>Rejections: </p>
-          <p>Personality Assessments: </p>
-          <p>Code Challenges: </p>
-          <p>HR Interviews: </p>
-          <p>Technical Interviews: </p>
-          <p>Internships: </p>
+          <p>Jobs Applied to: 251</p>
+          <p>Rejections: 127</p>
+          <p>Personality Assessments: 3</p>
+          <p>Code Challenges: 6</p>
+          <p>HR Interviews: 9</p>
+          <p>Technical Interviews: 3</p>
+          <p>Internships: 2</p>
+          <p>Non-Technical Tech Interview: 1</p>
+          <p>Third Round: 1</p>
         </div>
       </div>
     </div>

3c8eb792b392b530898c7f3b6d33dc66ca7cd33f
amandena <manda.m.johnson@gmail.com>
2021-01-27 23:19:06 -0500

    created redirect component

---
+++ client/src/components/Redirect404.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import {Redirect} from 'react-router-dom'
+
+const Redirect404 = () => {
+
+  const [redirecting, setRedirecting] = useState(false)
+
+  useEffect(() => {
+    const redirectTimeout = setTimeout(() => {
+      setRedirecting(true)
+    }, 3000)
+    return () => clearTimeout(redirectTimeout)
+  }, [redirecting, setRedirecting])
+
+  setTimeout(() => {
+    setRedirecting(true)
+  }, 3000)
+
+  return (
+    redirecting ? <Redirect push to="/" />
+    :
+    <div>
+      <h1 className="text-center">
+        404
+      </h1>
+      <p className="text-center">Taking you back home in 3 seconds...</p>
+    </div>
+  )
+
+}
+
+export default Redirect404

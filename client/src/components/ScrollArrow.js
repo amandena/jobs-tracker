662b060b4db8cdbd54adafea758630f59e64aca7
amandena <manda.m.johnson@gmail.com>
2021-02-01 23:38:32 -0500

    created arrow component

---
+++ client/src/components/ScrollArrow.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons'
+
+const ScrollArrow = () => {
+
+  const [showScroll, setShowScroll] = useState(false)
+
+  useEffect(()=> {
+    window.addEventListener('scroll', checkScrollTop)
+    return function cleanup() {
+      window.removeEventListener('scroll', checkScrollTop)
+    }
+  })
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 100){
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 100){
+      setShowScroll(false)
+    }
+  }
+
+  const scrollTop = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'})
+  }
+
+  return (
+    <div className='float-right'>
+      <i className="fas fa-arrow-circle-up fa-2x">
+        <FontAwesomeIcon 
+          icon={faArrowCircleUp} 
+          className='scrollTop' 
+          onClick={scrollTop} 
+          style={{display: showScroll ? 'flex' : 'none'}}
+        />
+      </i>
+    </div>
+  )
+}
+
+export default ScrollArrow

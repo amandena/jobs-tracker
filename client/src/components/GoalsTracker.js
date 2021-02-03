8d9e942c638940fe125cd73a1637534210294cac
amandena <manda.m.johnson@gmail.com>
2021-02-03 13:42:44 -0500

    separated sections into separate divs

--- client/src/components/GoalsTracker.js
+++ client/src/components/GoalsTracker.js
@@ -7,18 +7,47 @@ const GoalsTracker = () => {
       <div className='row spacer'>
         <div className='col-12'>
           <h3>Goals</h3>
-          <p>1 Applications for week of {new Date().toLocaleDateString()}</p>
-
-          <p>Jobs Applied to: 251</p>
-          <p>Rejections: 127</p>
-          <p>Personality Assessments: 3</p>
-          <p>Code Challenges: 6</p>
-          <p>HR Interviews: 9</p>
-          <p>Technical Interviews: 3</p>
-          <p>Internships: 2</p>
-          <p>Non-Technical Tech Interview: 1</p>
-          <p>Third Round: 1</p>
         </div>
+      </div>
+      <div className='col-12'>
+        <label>1 Applications for week of </label>
+        <p>{new Date().toLocaleDateString()}</p>
+      </div>
+      <div className='col-12'>
+        <label>Jobs Applied to:</label>
+        <p>251</p>
+      </div>
+      <div className='col-12'>
+        <label>Rejections:</label>
+        <p>127</p>
+      </div>
+      <div className='col-12'>
+        <label>Personality Assessments:</label>
+        <p>3</p>
+      </div>
+      <div className='col-12'>
+        <label>Code Challenges:</label>
+        <p>6</p>
+      </div>
+      <div className='col-12'>
+        <label>HR Interviews:</label>
+        <p>9</p>
+      </div>
+      <div className='col-12'>
+        <label>Technical Interviews:</label>
+        <p>3</p>
+      </div>
+      <div className='col-12'>
+        <label>Internships:</label>
+        <p>2</p>
+      </div>
+      <div className='col-12'>
+        <label>Non-Technical Tech Interview:</label>
+        <p>1</p>
+      </div>
+      <div className='col-12'>
+        <label>Third Round:</label>
+        <p>1</p>
       </div>
       <ScrollArrow/>
     </div>

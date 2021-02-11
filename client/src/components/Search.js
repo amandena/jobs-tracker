3a385d93812bfa8b0305d23749f6893521cfbf94
amandena <manda.m.johnson@gmail.com>
2021-02-11 14:58:07 -0500

    converted form to formik and yup validations

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,61 +1,65 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { updateSearchForm } from '../actions/updateSearchForm'
+import * as Yup from 'yup'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
 import { doSearch } from '../actions/doSearch'
 
-const Search = ({ searchForm, updateSearchForm, doSearch }) => {
-  const handleChange = e => {
-    const { name, value } = e.target
-    const updatedFormInfo = {
-      ...searchForm,
-      [name]: value
-    }
-    updateSearchForm(updatedFormInfo)
-  }
+const searchFormSchema = Yup.object().shape({
+  searchBox: Yup.string()
+})
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    doSearch(searchForm)
-  }
+const Search = ({ doSearch }) => {
 
   return(
     <div className='search container'>
       <div className='spacer'/>
-      <form onSubmit={handleSubmit}>
-        <div className='row form-group'>
-          <div className='col-md-8 offset-2 spacer-xs mb-2'>
-            <div className='form-floating'>
-              <input 
-                className='form-control input-lg'
-                id='searchFloat'
-                placeholder='Search'
-                type='text'
-                name='searchBox'
-                value={searchForm.searchBox}
-                onChange={handleChange}
-                autoComplete='off'
-              />
-              <label htmlFor='searchFloat'>Search</label>
+      <Formik
+        initialValues={{
+          searchBox: ''
+        }}
+        validationSchema={searchFormSchema}
+        onSubmit={async (values, {setStatus}) => {
+          const response = await doSearch(values)
+          if (response && response.status >= 300) {
+            setStatus({message: "Something went wrong. Please try again later."})
+          }
+        }}  
+      >
+        {({ touched, errors, status }) => (
+          <Form>
+            <div className='row form-group'>
+              <div className='col-md-8 offset-2 spacer-xs mb-2'>
+                <div className='form-floating'>
+                  <Field 
+                    className={`form-control input-lg ${touched.searchBox && errors.searchBox ? "is-invalid" : ""}`}
+                    id='searchFloat'
+                    placeholder='Search'
+                    type='text'
+                    name='searchBox'
+                    autoComplete='off'
+                  />
+                  <label htmlFor='searchFloat'>Search</label>
+                  <ErrorMessage name='searchBox' className='invalid-feedback' component='div'/>
+                </div>
+              </div>
+              <br/><br/>
+              {status && status.message ? <div className={`col-8 offset-2 text-center alert ${status >= 300 ? "alert-danger" : "alert-success"}`}>
+                {status.message}
+              </div> : null}
+              <div className='col-8 offset-2 spacer-xs justify-content-center'>
+                <Field 
+                  id='searchSubmit'
+                  className='btn btn-secondary mb-3'
+                  type='submit' 
+                  value='Search'
+                />
+              </div>
             </div>
-          </div>
-          <br/><br/>
-          <div className='col-8 offset-2 spacer-xs justify-content-center'>
-            <input 
-              className='btn btn-secondary mb-3'
-              type='submit' 
-              value='Search'
-            />
-          </div>
-        </div>
-      </form>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    searchForm: state.mainReducer.searchForm
-  }
-}
-
-export default connect(mapStateToProps, { updateSearchForm, doSearch })(Search)
+export default connect(null, { doSearch })(Search)

6b68cef5209979a98a9a9237ae489fd9b963a06a
amandena <manda.m.johnson@gmail.com>
2021-02-19 22:42:18 -0500

    put in a dummy job

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -3,7 +3,28 @@ import React from 'react'
 const Job = () => {
   return(
     <div className='job container'>
-      Job
+      <div className='row'>
+        <div className='col-12'>
+          <h3>Vitalcy</h3>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-12'>
+          <h5>Start Date: 9/15/20</h5>
+        </div>
+        <div className='col-12'>
+          <h5>Job Title: Internship</h5>
+        </div>
+        <div className='col-12'>
+          <h5>Application URL: N/A - LinkedIn</h5>
+        </div>
+        <div className='col-12'>
+          <h5>Referral Source: Medium Blog Post</h5>
+        </div>
+        <div className='col-12'>
+          <h5>Application Status: Hired!</h5>
+        </div>
+      </div>
     </div>
   )
 }

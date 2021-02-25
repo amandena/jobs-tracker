b8f94c5216ae907be14d910e6967cf3200918a71
amandena <manda.m.johnson@gmail.com>
2021-02-24 22:52:08 -0500

    implemented constants into container

--- client/src/containers/JobsContainer.js
+++ client/src/containers/JobsContainer.js
@@ -5,16 +5,17 @@ import Applications from '../components/Applications'
 import JobForm from '../components/JobForm'
 import ActivityTracker from '../components/ActivityTracker'
 import Redirect404 from '../components/Redirect404'
+import * as Constants from '../helpers/routeConstants'
 
 class JobsContainer extends React.Component {
   render() {
     return(
       <div className='jobs container'>
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/applications-tracker' component={Applications}/>
-          <Route path='/jobs/new' component={JobForm}/>
-          <Route path='/activity-tracker' component={ActivityTracker}/>
+          <Route exact path={`${Constants.HOME_ROUTE}`} component={Home}/>
+          <Route path={`${Constants.APPLICATIONS_TRACKER_ROUTE}`} component={Applications}/>
+          <Route path={`${Constants.NEW_JOB_ROUTE}`} component={JobForm}/>
+          <Route path={`${Constants.ACTIVITY_TRACKER_ROUTE}`} component={ActivityTracker}/>
           <Route path="*" component={Redirect404} />
         </Switch>
       </div>

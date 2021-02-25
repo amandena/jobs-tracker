30977d185734c3603783906a95b4774b6ee23dd0
amandena <manda.m.johnson@gmail.com>
2021-02-24 22:51:36 -0500

    created a constants file for routes

---
+++ client/src/helpers/routeConstants.js
@@ -0,0 +1,5 @@
+export const HOME_ROUTE = '/'
+
+export const APPLICATIONS_TRACKER_ROUTE = '/applications-tracker'
+export const NEW_JOB_ROUTE = '/jobs/new'
+export const ACTIVITY_TRACKER_ROUTE = '/activity-tracker'
